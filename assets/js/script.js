ec799e163233331d4d038e516d76d26506b4e2a7
mrsyafapri <rizkisyafapri@gmail.com>
2021-12-23 21:03:04 +0700

    pwa

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,30 +48,30 @@ AOS.init({
 // End:AOS
 
 // Start:GSAP
-gsap.registerPlugin(TextPlugin);
-gsap.to('.lead', {
-  duration: 1.5,
-  delay: 1.5,
-  text: 'Student | Web Developer | GDSC Lead'
-})
-gsap.from('.jumbotron img', {
-  duration: 1,
-  rotateY: 360,
-  opacity: 0,
-});
+// gsap.registerPlugin(TextPlugin);
+// gsap.to('.lead', {
+//   duration: 1.5,
+//   delay: 1.5,
+//   text: 'Student | Web Developer | GDSC Lead'
+// })
+// gsap.from('.jumbotron img', {
+//   duration: 1,
+//   rotateY: 360,
+//   opacity: 0,
+// });
 gsap.from('.navbar', {
   duration: 1.5,
   y: -150,
   opacity: 0,
   ease: 'bounce',
 });
-gsap.from('.display-4', {
-  duration: 1,
-  x: -50,
-  opacity: 0,
-  delay: 0.5,
-  ease: 'back',
-});
+// gsap.from('.display-4', {
+//   duration: 1,
+//   x: -50,
+//   opacity: 0,
+//   delay: 0.5,
+//   ease: 'back',
+// });
 // End:GSAP
 
 // Start:Footer Year
