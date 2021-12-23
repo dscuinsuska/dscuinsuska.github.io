005d92638a9af1aa3b117b715d2b97f38c4a86eb
Muhammad Rizki Syafapri <67809355+mrsyafapri@users.noreply.github.com>
2021-12-23 10:50:21 +0000

    Register service worker

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,20 @@
-// Start:Contact Form
-const scriptURL = 'https://script.google.com/macros/s/AKfycbx7lOidrnuyMbOFmrn7zRsit90nRJHmB6fKl0wQDpsPVmdW66RHAdxoMjIKEtARX5Uu/exec';
-const form = document.forms['mrsyafapri-contact-form'];
-const btnKirim = document.querySelector('.btn-kirim');
-const btnLoading = document.querySelector('.btn-loading');
-const myAlert = document.querySelector('.my-alert');
-
 fetch('https://raw.githubusercontent.com/dscuinsuska/members/main/memberData.json')
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        appendData(data);
-    })
-    .catch(function (err) {
-        console.log('error: ' + err);
-    });
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    appendData(data);
+  })
+  .catch(function (err) {
+    console.log('error: ' + err);
+  });
 
 function appendData(data) {
-    var mainContainer = document.getElementById("myData");
-    data.forEach(element => {
-        var div = document.createElement("div");
-        div.className = "col-md-4 mb-3"
-        div.innerHTML = `
+  var mainContainer = document.getElementById("myData");
+  data.forEach(element => {
+    var div = document.createElement("div");
+    div.className = "col-md-4 mb-3"
+    div.innerHTML = `
                 <div class="card">
                     <div class="card-body text-center">
                         <h5 class="card-title">${element.nama}</h5>
@@ -30,41 +23,54 @@ function appendData(data) {
                     </div>
                 </div>
         `;
-        mainContainer.appendChild(div);
+    mainContainer.appendChild(div);
+  });
+}
+
+// PWA
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('./assets/js/sw.js').then(function (registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function (err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
     });
+  });
 }
 
 // Start:AOS
 AOS.init({
-    once: true,
-    duration: 1000,
+  once: true,
+  duration: 1000,
 });
 // End:AOS
 
 // Start:GSAP
 gsap.registerPlugin(TextPlugin);
 gsap.to('.lead', {
-    duration: 1.5,
-    delay: 1.5,
-    text: 'Student | Web Developer | GDSC Lead'
+  duration: 1.5,
+  delay: 1.5,
+  text: 'Student | Web Developer | GDSC Lead'
 })
 gsap.from('.jumbotron img', {
-    duration: 1,
-    rotateY: 360,
-    opacity: 0,
+  duration: 1,
+  rotateY: 360,
+  opacity: 0,
 });
 gsap.from('.navbar', {
-    duration: 1.5,
-    y: -150,
-    opacity: 0,
-    ease: 'bounce',
+  duration: 1.5,
+  y: -150,
+  opacity: 0,
+  ease: 'bounce',
 });
 gsap.from('.display-4', {
-    duration: 1,
-    x: -50,
-    opacity: 0,
-    delay: 0.5,
-    ease: 'back',
+  duration: 1,
+  x: -50,
+  opacity: 0,
+  delay: 0.5,
+  ease: 'back',
 });
 // End:GSAP
 
