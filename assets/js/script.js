852b32fef6532c3dfd5b661ae563605f61a8b708
mrsyafapri <rizkisyafapri@gmail.com>
2021-12-24 06:31:07 +0700

    pwa

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,7 +30,7 @@ function appendData(data) {
 // PWA
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function () {
-    navigator.serviceWorker.register('./assets/js/sw.js').then(function (registration) {
+    navigator.serviceWorker.register('sw.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function (err) {
