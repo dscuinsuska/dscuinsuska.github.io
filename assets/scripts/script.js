93a1b336b1c309b613f35714014d860ea3c5c314
mrsyafapri <rizkisyafapri@gmail.com>
2022-01-16 18:01:27 +0700

    navbar update

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -60,18 +60,18 @@ function appendData(data) {
   });
 }
 
-// PWA
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function () {
-    navigator.serviceWorker.register('sw.js').then(function (registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function (err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
+// // PWA Off jika mode development
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', function () {
+//     navigator.serviceWorker.register('sw.js').then(function (registration) {
+//       // Registration was successful
+//       console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//     }, function (err) {
+//       // registration failed :(
+//       console.log('ServiceWorker registration failed: ', err);
+//     });
+//   });
+// }
 
 // Start:AOS
 AOS.init({
