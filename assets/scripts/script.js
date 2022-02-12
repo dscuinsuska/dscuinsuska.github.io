1cea196eb0538e23109dd03897927541f505f8e0
mrsyafapri <rizkisyafapri@gmail.com>
2022-02-12 11:01:34 +0700

    update members

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -14,16 +14,22 @@ function appendData(data) {
   const coreContainer = document.getElementById("core");
   data.core.forEach(element => {
     const div = document.createElement("div");
-    div.className = "col-md-4 mb-3"
+    div.className = "col-md-3 mb-3"
     div.innerHTML = `
-                <div class="card">
+                  <div class="card shadow-lg">
                     <div class="card-body text-center">
-                        <h5 class="card-title">${element.nama}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">${element.nim}</h6>
-                        <p class="card-text">${element.jurusan}</p>                    
+                      <img src="${element.foto}" class="img-fluid rounded-circle p-3" alt="${element.nama}">
+                      <p class="card-title fs-5 fw-bold">${element.nama}</p>
+                      <p class="card-subtitle mb-1">${element.role}</p>
+                      <p class="card-text text-muted">${element.jurusan}</p>
+                      <a href="#" target="_blank" rel="noopener noreferrer" class="text-decoration-none px-2">
+                        <i class="fa fa-instagram"></i>
+                      </a>
+                      <a href="#" target="_blank" rel="noopener noreferrer" class="text-decoration-none px-2">
+                        <i class="fa fa-linkedin"></i>
+                      </a>
                     </div>
-                </div>
-        `;
+                  </div>`;
     coreContainer.appendChild(div);
   });
   // Expert Team Data
@@ -32,12 +38,21 @@ function appendData(data) {
     const div = document.createElement("div");
     div.className = "col-md-4 mb-3"
     div.innerHTML = `
-                <div class="card">
-                    <div class="card-body text-center">
-                        <h5 class="card-title">${element.nama}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">${element.nim}</h6>
-                        <p class="card-text">${element.bidang}</p>                    
+                <div class="card shadow-lg">
+                  <div class="card-body text-center">
+                    <div class="row">
+                      <div class="col-md-3">
+                        <img src="${element.foto}" class="rounded-circle" alt="${element.nama}">
+                      </div>
+                      <div class="col-md-9">
+                        <div class="card-body">
+                          <p class="card-title fs-5 fw-bold">${element.nama}</p>
+                          <p class="card-subtitle mb-1">${element.bidang}</p>
+                          <p class="card-text text-muted">${element.nim}</p>
+                        </div>
+                      </div>
                     </div>
+                  </div>
                 </div>
         `;
     expertContainer.appendChild(div);
@@ -45,18 +60,13 @@ function appendData(data) {
   // Members Data
   const memberContainer = document.getElementById("members");
   data.members.forEach(element => {
-    const div = document.createElement("div");
-    div.className = "col-md-4 mb-3"
-    div.innerHTML = `
-                <div class="card">
-                    <div class="card-body text-center">
-                        <h5 class="card-title">${element.nama}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">${element.nim}</h6>
-                        <p class="card-text">${element.jurusan + ' ' + element.universitas}</p>                    
-                    </div>
-                </div>
-        `;
-    memberContainer.appendChild(div);
+    const tr = document.createElement("tr");
+    tr.innerHTML = `    
+          <td>${element.nama}</td>
+          <td>${element.nim}</td>
+          <td>${element.jurusan + ' ' + element.universitas}</td>
+    `;
+    memberContainer.appendChild(tr);
   });
 }
 
@@ -106,9 +116,3 @@ gsap.from('.navbar', {
 //   ease: 'back',
 // });
 // End:GSAP
-
-// Start:Footer Year
-const date = new Date();
-const footerYear = document.getElementById("footer-year");
-footerYear.innerHTML = date.getFullYear();
-// End:Footer Year
